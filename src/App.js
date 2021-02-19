c31e4dd3d4996274435f53bb750a3023a279c536
Yannick Gloster <yannickgloster@gmail.com>
2021-02-19 15:56:40 +0000

    Updated hompage to include new devices table

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
-import ProTip from './ProTip';
+import DevicesTable from './DevicesTable';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="http://localhost:3000/">
+        IoT-Powered Pandemic Safety Suite
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -23,9 +23,15 @@ export default function App() {
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v4-beta example
+          IoT-Powered Pandemic Safety Suite
         </Typography>
-        <ProTip />
+        <Typography variant="h5" component="h1" gutterBottom>
+          People: 20
+        </Typography>
+        <Typography variant="h5" component="h1" gutterBottom>
+          Max: 20
+        </Typography>
+        <DevicesTable />
         <Copyright />
       </Box>
     </Container>
