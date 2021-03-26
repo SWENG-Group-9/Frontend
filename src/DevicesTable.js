4548f739f80360c8c7e9026051e247e826202991
Yannick Gloster <yannickgloster@gmail.com>
2021-03-26 09:02:10 +0000

    Updated formatting of the table

--- src/DevicesTable.js
+++ src/DevicesTable.js
@@ -13,31 +13,24 @@ import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import Battery80Icon from "@material-ui/icons/Battery80";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import LockOpenOutlinedIcon from "@material-ui/icons/LockOpenOutlined";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
-    maxWidth: 360,
     backgroundColor: theme.palette.background,
   },
 }));
 
 export default function DevicesTable() {
   const classes = useStyles();
-  const [checked, setChecked] = React.useState(["wifi"]);
+  let locked = true;
 
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
+  const handleLock = (value) => () => {
+    locked = !locked;
+    console.log(locked);
   };
 
   return (
@@ -54,57 +47,34 @@ export default function DevicesTable() {
           </Fab>
         </Box>
       </Box>
-      <List className={classes.root}>
-        <ListItem>
-          <ListItemText
-            disableTypography
-            id="switch-list-label-entrance"
-            primary={
-              <Typography variant="h4" style={{ color: "#1a535c" }}>
-                Entrance
-              </Typography>
-            }
-          />
-          <ListItemSecondaryAction>
-            <Switch
-              edge="end"
-              onChange={handleToggle("entrance")}
-              checked={checked.indexOf("entrance") !== -1}
-              inputProps={{ "aria-labelledby": "switch-list-label-entrance" }}
+      <Box flexGrow={1}>
+        <List className={classes.root}>
+          <ListItem>
+            <ListItemText
+              disableTypography
+              id="switch-list-label-entrance"
+              primary={
+                <Typography variant="h4" style={{ color: "#1a535c" }}>
+                  Frontdoor
+                </Typography>
+              }
+              secondary={
+                <Typography variant="h6" style={{ color: "#1a535c" }}>
+                  Entrance
+                </Typography>
+              }
             />
-            <IconButton edge="end" aria-label="delete">
-              <DeleteIcon />
-            </IconButton>
-            <IconButton edge="end" aria-label="Battery">
-              <Battery80Icon />
-            </IconButton>
-          </ListItemSecondaryAction>
-        </ListItem>
-        <ListItem>
-          <ListItemText
-            id="switch-list-label-exit"
-            primary={
-              <Typography variant="h4" style={{ color: "#1a535c" }}>
-                Exit
-              </Typography>
-            }
-          />
-          <ListItemSecondaryAction>
-            <Switch
-              edge="end"
-              onChange={handleToggle("exit")}
-              checked={checked.indexOf("exit") !== -1}
-              inputProps={{ "aria-labelledby": "switch-list-label-exit" }}
-            />
-            <IconButton edge="end" aria-label="delete">
-              <DeleteIcon />
-            </IconButton>
-            <IconButton edge="end" aria-label="Battery">
-              <Battery80Icon />
-            </IconButton>
-          </ListItemSecondaryAction>
-        </ListItem>
-      </List>
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="LockIcon" onClick={handleLock}>
+                {locked ? <LockOutlinedIcon /> : <LockOpenOutlinedIcon />}
+              </IconButton>
+              <IconButton edge="end" aria-label="MoreVertIcon">
+                <MoreVertIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </List>
+      </Box>
     </>
   );
 }
