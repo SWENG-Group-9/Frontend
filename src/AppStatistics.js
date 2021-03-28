4894a6df0e2a1db25aba0fd1cd13d593dfae8c44
Arynel <54998052+Arynel@users.noreply.github.com>
2021-03-28 14:01:01 +0100

    finished stats graph
    fully functioning statistics graph with time and date, also included a year's worth of random data in data.json

--- src/AppStatistics.js
+++ src/AppStatistics.js
@@ -1,6 +1,5 @@
 import { Container , Grid} from '@material-ui/core'
-import {useState, useEffect} from 'react';
-import Typography from '@material-ui/core/Typography'
+import {useState, useEffect} from 'react'
 import ChartControl from './ChartControl'
 import StatCharts from './StatCharts'
 
@@ -9,20 +8,29 @@ export default function AppStatistics(){
     const [times, setTimes] = useState([]); 
     const [values, setValues] = useState([]);
     const [data, setData] = useState([]);
+    const [found, setFound] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [dateL, setDateL] = useState("");
 
     useEffect(() => {
-        getPeriodDataValues();  
-    },[data])
+        getPeriodDataValues();
+    },[loading])
 
-    function fetchData(mode, date="01/01/2021", start="00:00", end="07:00"){
-    
-        fetch("http://localhost:8000/data").then(
+    function fetchData(mode, date, start, end){
+        setLoading(true);
+        setDateL(date);
+        fetch("http://localhost:8000/"+date)
+        .then(
             (resp) => {
+                if(!resp.ok){
+                    setFound(false);
+                    throw Error("Could not find data for this period");
+                }
                 return resp.json();
-            }
-        ).then(
+        })
+        .then(
             (rawData) => {
-                let dateS = date.split("/")
+                let dateS = date.split("-")
                 let startS = start.split(":")
                 let endS = end.split(":")
                 for (let i = 0;i<2;i++){
@@ -32,27 +40,31 @@ export default function AppStatistics(){
                 }
 
                 startS[1] = Math.ceil(startS[1] / 30) * 30;
-                console.log(startS[1])
 
-                let times=[];
-                let time = new Date(dateS[2], dateS[1], dateS[0], startS[0], startS[1]);
-                let endT = new Date(dateS[2], dateS[1], dateS[0], endS[0], endS[1], 1);
+                let timesar=[];
+                let time = new Date(dateS[0], dateS[1], dateS[2], startS[0], startS[1]);
+                let endT = new Date(dateS[0], dateS[1], dateS[2], endS[0], endS[1], 1);
 
                 while(time < endT){
-                    times.push(("0"+time.getHours()).slice(-2)+":"+("0"+time.getMinutes()).slice(-2)); 
+                    timesar.push(("0"+time.getHours()).slice(-2)+":"+("0"+time.getMinutes()).slice(-2)); 
                     time.setMinutes(time.getMinutes()+mode)   
                 }
 
                 let dataobj = []
-                times.forEach((item)=>{
+                timesar.forEach((item)=>{
                     let val = rawData.find((ob) => item ==ob.time)
                     if(val!=undefined){
                         dataobj.push(val)
                     }
                 });
                 setData(dataobj)
-            }
-        )
+                setFound(true);
+                setLoading(false);
+        })
+        .catch((err) => {
+            console.log(err.message)
+            setLoading(false);
+        })
     }
 
 
@@ -78,11 +90,11 @@ export default function AppStatistics(){
     return (
         <Container style={{padding:0}}>
             <Grid style={{paddingTop:30}} container spacing={1}>
-                <Grid item xs={4} sm={3}>
+                <Grid item xs={4} md={3}>
                     <ChartControl  setPeriods={getPeriodDataValues}/>
                 </Grid>
                 <Grid item xs={8} sm={8}>
-                    <StatCharts values={values} times={times} />
+                    <StatCharts loading={loading} date={dateL} found={found} values={values} times={times}/>
                 </Grid>
             </Grid>
         </Container>
--- src/ChartControl.js
+++ src/ChartControl.js
@@ -5,9 +5,9 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme)=>({
     gridStyle:{
         margin:3,
-        [theme.breakpoints.up('md')]: {
+        [theme.breakpoints.up('lg')]: {
             margin:0,
-            paddingLeft:15
+            paddingLeft:10
         },
     },
     buttonStyle:{
@@ -17,7 +17,7 @@ const useStyles = makeStyles((theme)=>({
         },
     },
     periodText:{
-        fontSize:20,
+        fontSize:18,
         paddingTop:15,
         fontStyle:"oblique"
     }
@@ -30,8 +30,12 @@ export default function ChartControl({setPeriods}){
     const periodChoices = ["30 Minutes", "1 Hour", "2 Hours", "3 Hours"];
     const [periodValue, setPeriodValue] = useState([0,"30 Minutes"]);
     const [startTime, setStartTime] = useState("00:00");
-    const [endTime, setEndTime] = useState("07:00");
-    const [dataDate, setDataDate] = useState("01/01/2021");
+    const [endTime, setEndTime] = useState("23:30");
+    const [dataDate, setDataDate] = useState("2021-01-01");
+
+    useEffect(()=>{
+        setPeriods(periodValue[1],dataDate, startTime,endTime)
+    },[periodValue, dataDate, startTime, endTime])
 
     const setPeriod = () => {
         let index = periodValue[0];
@@ -44,50 +48,58 @@ export default function ChartControl({setPeriods}){
         setPeriodValue([index,periodChoices[index]]);
     }
 
-    useEffect(()=>{
-        setPeriods(periodValue[1],dataDate, startTime,endTime)
-    },[periodValue, dataDate, startTime, endTime])
-
+    
 
     return(
         <Container>
-            <Grid direction="column" alignItems="center" container spacing={5} 
-                    className={classes.gridStyle}>
-                    <Grid item>
-                        <Button color="secondary" onClick={setPeriod} variant="contained">Period</Button>
-                        <Typography className={classes.periodText} variant="h6" color="secondary" align="center">
+            <Grid direction="column" alignItems="center" container spacing={4} 
+                className={classes.gridStyle}>
+                <Grid item>
+                    <Button color="secondary" onClick={setPeriod} variant="contained">Period</Button>
+                    <Typography className={classes.periodText} variant="h6" color="secondary" align="center">
                             {periodValue[1]}
+                    </Typography>
+                </Grid>
+                <Grid item xs>
+                    <TextField
+                        value={startTime}
+                        onChange={(e) => setStartTime(e.target.value)}
+                        label="Start"
+                        type="time"
+                        InputLabelProps={{
+                        shrink: true,
+                    }}/>
+                </Grid>
+                <Grid item xs>
+                    <TextField
+                        value={endTime}
+                        onChange={(e) => setEndTime(e.target.value)}
+                        label="End"
+                        type="time"
+                        InputLabelProps={{
+                        shrink: true,
+                    }}/>
+                </Grid>
+                <Grid item xs>
+                    {(startTime>endTime)&&
+                        <Typography variant="h5" color="initial">
+                            This date selection is invalid
                         </Typography>
-                    </Grid>
-                    <Grid style={{paddingLeft:40}} container spacing={4}>
-                        <Grid item xs>
-                            <TextField
-                                value={startTime}
-                                onChange={(e) => setStartTime(e.target.value)}
-                                label="Start"
-                                type="time"
-                                InputLabelProps={{
-                                shrink: true,
-                            }}/>
-                        </Grid>
-                        <Grid item xs>
-                            <TextField
-                                value={endTime}
-                                onChange={(e) => setEndTime(e.target.value)}
-                                label="End"
-                                type="time"
-                                InputLabelProps={{
-                                shrink: true,
-                            }}/>
-                        </Grid>
-                        <Grid item xs>
-                            {(startTime>endTime)&&
-                                <Typography variant="h5" color="initial">
-                                    This date selection is invalid
-                                </Typography>
-                            }
-                        </Grid>
-                    </Grid>
+                    }
+                </Grid>
+                <Grid item xs>
+                    <TextField
+                        style={{width:140}}
+                        value={dataDate}
+                        id="date"
+                        label="Select Date"
+                        type="date"
+                        onChange={(e)=>setDataDate(e.target.value)}
+                        className={classes.textField}
+                        InputLabelProps={{
+                        shrink: true,
+                    }}/>   
+                </Grid>
             </Grid>
         </Container>
     )
