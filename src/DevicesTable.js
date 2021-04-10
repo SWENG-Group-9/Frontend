c996d5a370261c2617462a5090cc72f10d79210f
Yannick Gloster <yannickgloster@gmail.com>
2021-04-10 17:26:04 +0100

    Removed pop over
    Might add back later

--- src/DevicesTable.js
+++ src/DevicesTable.js
@@ -9,15 +9,10 @@ import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 import LockOpenOutlinedIcon from "@material-ui/icons/LockOpenOutlined";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import ClickAwayListener from "@material-ui/core/ClickAwayListener";
-import Grow from "@material-ui/core/Grow";
-import Paper from "@material-ui/core/Paper";
-import Popper from "@material-ui/core/Popper";
 import MenuItem from "@material-ui/core/MenuItem";
-import MenuList from "@material-ui/core/MenuList";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -25,8 +20,6 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Button from "@material-ui/core/Button";
-import Select from "@material-ui/core/Select";
-import InputLabel from "@material-ui/core/InputLabel";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -49,7 +42,6 @@ const tempDevices = [
 export default function DevicesTable() {
   const classes = useStyles();
   const [locked, setLocked] = React.useState(["locked"]);
-  const [open, setOpen] = React.useState(false);
   const [addOpen, setAddOpen] = React.useState(false);
   const [type, setType] = React.useState("");
 
@@ -78,36 +70,7 @@ export default function DevicesTable() {
     setLocked(newLock);
   };
 
-  const anchorRef = React.useRef(null);
-
-  const handleOpen = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
-
-  const handleClose = (event) => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
-
-    setOpen(false);
-  };
-
-  function handleListKeyDown(event) {
-    if (event.key === "Tab") {
-      event.preventDefault();
-      setOpen(false);
-    }
-  }
-
-  // return focus to the button when we transitioned from !open -> open
-  const prevOpen = React.useRef(open);
-  React.useEffect(() => {
-    if (prevOpen.current === true && open === false) {
-      anchorRef.current.focus();
-    }
-
-    prevOpen.current = open;
-  }, [open]);
+  const handleDelete = (value) => {};
 
   return (
     <>
@@ -195,52 +158,8 @@ export default function DevicesTable() {
                     <LockOpenOutlinedIcon />
                   )}
                 </IconButton>
-                <IconButton
-                  edge="end"
-                  aria-label="MoreVertIcon"
-                  aria-haspopup="true"
-                  aria-controls={open ? "menu-list-grow" : undefined}
-                  onClick={handleOpen}
-                  ref={anchorRef}
-                >
-                  <MoreVertIcon />
-                  <Popper
-                    open={open}
-                    anchorEl={anchorRef.current}
-                    role={undefined}
-                    transition
-                    disablePortal
-                    placement="right"
-                  >
-                    {({ TransitionProps, placement }) => (
-                      <Grow
-                        {...TransitionProps}
-                        style={{
-                          transformOrigin:
-                            placement === "bottom"
-                              ? "center top"
-                              : "center bottom",
-                        }}
-                      >
-                        <Paper>
-                          <ClickAwayListener onClickAway={handleClose}>
-                            <MenuList
-                              autoFocusItem={open}
-                              id="menu-list-grow"
-                              onKeyDown={handleListKeyDown}
-                            >
-                              <MenuItem onClick={handleClose}>
-                                Manage Device
-                              </MenuItem>
-                              <MenuItem onClick={handleClose}>
-                                Door Statistics
-                              </MenuItem>
-                            </MenuList>
-                          </ClickAwayListener>
-                        </Paper>
-                      </Grow>
-                    )}
-                  </Popper>
+                <IconButton>
+                  <DeleteOutlineIcon onclick={handleLock(index)} />
                 </IconButton>
               </ListItemSecondaryAction>
             </ListItem>
